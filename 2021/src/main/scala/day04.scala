5ee1332ffde0baa455b521d81e6776de8dff27d2
fbraza <fbraza@tutanota.com>
2021-12-14 20:26:44 +0100

    adventofcode: refactor day04

--- 2021/src/main/scala/day04.scala
+++ 2021/src/main/scala/day04.scala
@@ -16,82 +16,49 @@ object giantSquid extends App {
       .toList
     val boards = data
       .filter(x => x.size <= 14 && x != "")
-      .map(row => row.split(" ").filter(ele => ele != "").toList.map(ele => ele.toInt))
+      .map(row =>
+        row.split(" ").filter(ele => ele != "").toList.map(ele => ele.toInt)
+      )
       .sliding(5, 5)
       .toList
     (numbers, boards)
   }
 
   def findFirstWinningBoard(
-  numbers: List[Int],
-  boards: List[List[List[Int]]],
-  memo: mutable.Map[Int, Boolean] = INIMEMO,
-  previousNumber: Int = 0
+      numbers: List[Int],
+      boards: List[List[List[Int]]],
+      memo: mutable.Map[Int, Boolean] = INIMEMO,
+      previousNumber: Int = 0
   ): Int = {
     val currentNumber = numbers(0)
     val updatedMemo = memo
     val updatedNumbers = numbers.drop(1)
     val winningBoard = boards.filter(board => isBingo(board))
-    if (winningBoard.size > 0) return sumBoard(winningBoard(0)) * previousNumber
-    val updatedBoards = boards
-      .map(
-        board => board.map(
-          row => row.map(
-            num => {
-              if (num < 0) {
-                num
-              } else if (updatedMemo(num)) {
-                num + NEGATOR
-              } else if (!updatedMemo(num) && num == currentNumber) {
-                updatedMemo(num) = true
-                num + NEGATOR
-              } else {
-                num
-              }
-            }
-          )
-        )
-      )
+    if (winningBoard.size > 0)
+      return sumBoard(winningBoard(0)) * previousNumber
+    val updatedBoards = update(boards, updatedMemo, currentNumber)
     findFirstWinningBoard(updatedNumbers, updatedBoards, updatedMemo, currentNumber)
   }
 
-
   def findLastWinningBoard(
-  numbers: List[Int],
-  boards: List[List[List[Int]]],
-  memo: mutable.Map[Int, Boolean] = INIMEMO,
-  previousNumber: Int = 0
+      numbers: List[Int],
+      boards: List[List[List[Int]]],
+      memo: mutable.Map[Int, Boolean] = INIMEMO,
+      previousNumber: Int = 0
   ): Int = {
     val currentNumber = numbers(0)
     val updatedMemo = memo
     val updatedNumbers = numbers.drop(1)
-    if (boards.size == 1 && boards.exists(board => isBingo(board))) return sumBoard(boards(0)) * previousNumber
+    if (boards.size == 1 && boards.exists(board => isBingo(board)))
+      return sumBoard(boards(0)) * previousNumber
     val notWinningBoards = boards.filter(board => !isBingo(board))
-    val updatedBoards = notWinningBoards
-      .map(
-        board => board.map(
-          row => row.map(
-            num => {
-              if (num < 0) {
-                num
-              } else if (updatedMemo(num)) {
-                num + NEGATOR
-              } else if (!updatedMemo(num) && num == currentNumber) {
-                updatedMemo(num) = true
-                num + NEGATOR
-              } else {
-                num
-              }
-            }
-          )
-        )
-      )
+    val updatedBoards = update(notWinningBoards, updatedMemo, currentNumber)
     findLastWinningBoard(updatedNumbers, updatedBoards, updatedMemo, currentNumber)
   }
 
   def isBingo(board: List[List[Int]]): Boolean = {
-    val areRowBingo = board.exists(row => row.forall( num => num < 0))
-    val areColBingo = board.transpose.exists(row => row.forall( num => num < 0))
+    val areRowBingo = board.exists(row => row.forall(num => num < 0))
+    val areColBingo = board.transpose.exists(row => row.forall(num => num < 0))
     areRowBingo || areColBingo
   }
 
@@ -105,5 +72,30 @@ object giantSquid extends App {
     val memo = mutable.Map[Int, Boolean]()
     for (number <- numbers) { memo(number) = false }
     memo
+  }
+
+  def update(
+      boards: List[List[List[Int]]],
+      memo: mutable.Map[Int, Boolean],
+      current: Int
+  ):List[List[List[Int]]] = {
+    val result = boards
+      .map(board =>
+        board.map(row =>
+          row.map(num => {
+            if (num < 0) {
+              num
+            } else if (memo(num)) {
+              num + NEGATOR
+            } else if (!memo(num) && num == current) {
+              memo(num) = true
+              num + NEGATOR
+            } else {
+              num
+            }
+          })
+        )
+      )
+    result
   }
 }
